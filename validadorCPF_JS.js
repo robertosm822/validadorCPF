5fdfbad58f5e1d1ef6308882aa70fb269e6f091d
Roberto Melo <robertomelo822@gmail.com>
2014-12-12 11:17:34 -0200

    Criação do primeiro arquivo.
    Função de validação de CPF.

---
+++ validadorCPF_JS.js
@@ -0,0 +1,73 @@
+/* Valida CPF jQuery */
+            jQuery.fn.validacpf = function(){ 
+					this.change(function(){
+						CPF = jQuery(this).val();
+						if(!CPF){ return false;}
+						erro  = new String;
+						cpfv  = CPF;
+						if(cpfv.length == 14 || cpfv.length == 11){
+							cpfv = cpfv.replace('.', '');
+							cpfv = cpfv.replace('.', '');
+							cpfv = cpfv.replace('-', '');
+				  
+							var nonNumbers = /\D/;
+					
+							if(nonNumbers.test(cpfv)){
+								erro = "A verificacao de CPF suporta apenas números!";
+							}else{
+								if (cpfv == "00000000000" ||
+									cpfv == "11111111111" ||
+									cpfv == "22222222222" ||
+									cpfv == "33333333333" ||
+									cpfv == "44444444444" ||
+									cpfv == "55555555555" ||
+									cpfv == "66666666666" ||
+									cpfv == "77777777777" ||
+									cpfv == "88888888888" ||
+									cpfv == "99999999999") {
+											
+									erro = "Número de CPF inválido!"
+								}
+								var a = [];
+								var b = new Number;
+								var c = 11;
+				  
+								for(i=0; i<11; i++){
+									a[i] = cpfv.charAt(i);
+									if (i < 9) b += (a[i] * --c);
+								}
+								if((x = b % 11) < 2){
+									a[9] = 0
+								}else{
+									a[9] = 11-x
+								}
+								b = 0;
+								c = 11;
+								for (y=0; y<10; y++) b += (a[y] * c--);
+					
+								if((x = b % 11) < 2){
+									a[10] = 0;
+								}else{
+									a[10] = 11-x;
+								}
+								if((cpfv.charAt(9) != a[9]) || (cpfv.charAt(10) != a[10])){
+									erro = "Número de CPF inválido.";
+								}
+							}
+						}else{
+							if(cpfv.length == 0){
+								return false;
+							}else{
+								erro = "Número de CPF inválido.";
+							}
+						}
+						if (erro.length > 0){
+							jQuery(this).val('');
+							jQuery('.cpf_box').append("<div style='font-size:12px; color:red;'>"+erro+"</div>");
+							setTimeout(function(){jQuery(this).focus();},100);
+							return false;
+						}
+						return jQuery(this);
+					});
+				}
+			jQuery("#mgm_register_field_cpf").validacpf();
